18d17466a8fe0be4288139ade56b2b3770641e48
stephan.com <stephan@stephan.com>
2022-10-20 01:45:31 -0700

    hack architecture flags for M1/M2 macs
    might be better to use something like `/armv?\d{,2}s?/`

--- src/ruby/ext/grpc/extconf.rb
+++ src/ruby/ext/grpc/extconf.rb
@@ -77,8 +77,8 @@ ENV['EMBED_CARES'] = 'true'
 
 ENV['ARCH_FLAGS'] = RbConfig::CONFIG['ARCH_FLAG']
 if apple_toolchain && !cross_compiling
-  if RUBY_PLATFORM =~ /arm64/
-    ENV['ARCH_FLAGS'] = '-arch arm64'
+  if RUBY_PLATFORM =~ /arm(v[67]s?|64)/
+    ENV['ARCH_FLAGS'] = '-arch ' + RUBY_PLATFORM
   else
     ENV['ARCH_FLAGS'] = '-arch i386 -arch x86_64'
   end
